2d2d43a7d4c073088f375cebe1f09f4926dcb07f
vinwik <50736716+vinwik@users.noreply.github.com>
2020-01-21 00:35:28 +0000

    first commit

---
+++ date.js
@@ -0,0 +1,59 @@
+function startTime() {
+    const clock = new Date();
+
+    const option1 = { hour: '2-digit', minute: '2-digit' };
+    const option2 = { second: '2-digit' }; //Does not display 2 digits
+    
+    time = clock.toLocaleTimeString([], option1);
+    second = clock.toLocaleTimeString([], option2); 
+
+    console.log(time);
+
+    // hours = clock.getHours();
+    // minutes = clock.getMinutes()
+    // second = clock.getSeconds();
+
+    second = checkTime(second)
+    
+    document.querySelector('.time').textContent = time;
+    document.querySelector('.second').textContent = second;
+
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};
+        return i;
+    }
+}
+
+function startDate() {
+    const today = new Date();
+
+    const option3 = { weekday: 'long', day: 'numeric', month: 'long' };
+    
+    date = today.toLocaleDateString([], option3);
+    
+    document.querySelector('.date').textContent = date;
+
+}
+
+function greeting() {
+    const greet = new Date();
+    const hours = greet.getHours();
+    
+    if ( hours < 12){
+        document.querySelector('.greeting').textContent = 'Good Morning';
+     } else if (hours < 23 ){
+        document.querySelector('.greeting').textContent = 'Good Afternoon';
+     }  else {
+        document.querySelector('.greeting').textContent = 'Good Evening';
+     }
+}
+
+function startAll() {
+    startTime();
+    startDate();
+    greeting();
+}
+
+window.onload = startAll();
+
+setInterval(startAll, 1000);
---
+++ weather.js
@@ -0,0 +1,59 @@
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=14583dc926375985c30e1b4f2c7f4d23'
+
+async function getData() {
+   const response = await fetch(url);
+   const data = await response.json();
+
+   let weather = data.weather[0].main.toLowerCase(); // lower case weather description
+   let temp = data.main.temp; 
+   temp = Math.round(temp); // No decimal, Integer
+
+   console.log(temp, weather);
+
+   document.querySelector('.temp').textContent = temp + '°C';
+   document.querySelector('.weather-icon').src = 'svg/' + weather + '.svg';
+   document.querySelector('.bg').style.backgroundImage = 'url(img/' + weather + '.jpg)';
+
+   const getHours = new Date();
+   let hours = getHours.getHours();
+
+   if (weather === 'clear' && hours >= 20 || hours < 6 ){
+      document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'night' + '.jpg)';
+   } else if (weather === 'clear' &&  hours < 12 ){
+      document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'morning' + '.jpg)';
+   }  else if (weather === 'clear' && hours >= 17){
+      document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'evening' + '.jpg)';
+   }
+};
+
+window.onload = getData();
+
+setInterval(getData, 1000);
+
+
+// /*---------- TEST WITH NO API ----------*/
+// let weather = 'clear'
+// let temp = 1
+
+// document.querySelector('.temp').textContent = temp + '°C';
+// document.querySelector('.weather-icon').src = 'svg/' + weather + '.svg';
+// document.querySelector('.bg').style.backgroundImage = 'url(img/' + weather + '.jpg)';
+
+// const getHours = new Date();
+// let hours = getHours.getHours();
+
+// hours = 12
+
+// if (weather === 'clear' && hours >= 20 || hours < 6 ){
+//    document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'night' + '.jpg)';
+// } else if (hours < 9 ){
+//    document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'morning' + '.jpg)';
+// }  else if (hours >= 17){
+//    document.querySelector('.bg').style.backgroundImage = 'url(img/' + 'evening' + '.jpg)';
+// }
+
+
+
+
+
+
